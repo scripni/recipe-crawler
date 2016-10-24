40f684019c711ec846a56e238d474047a64a45aa
Andrei Scripniciuc <andreisc@microsoft.com>
2016-10-25 00:14:52 +0100

    removed unnecessary method

--- app/core/Crawler.cs
+++ app/core/Crawler.cs
@@ -18,20 +18,5 @@ namespace RecipeCrawler.Core
                 Console.WriteLine("Got response");
             }
         }
-
-        private async Task<byte[]> ReadAsync(Stream responseStream)
-        {
-            byte[] buffer = new byte[32 * 1024];
-            using (MemoryStream memStream = new MemoryStream())
-            {
-                int bytesRead;
-                while ((bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                {
-                    memStream.Write(buffer, 0, bytesRead);
-                }
-
-                return memStream.ToArray();
-            }
-        }
     }
 }
